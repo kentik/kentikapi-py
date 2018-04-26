4e8ae31dd105e39bdfe95f36647309b818fb69e3
Stefan Praszalowicz <stefan@kentik.com>
2018-04-26 13:52:58 -0700

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kentikapi',
-    version='0.1.0',
+    version='0.1.2',
     author='Blake Caldwell',
     packages=find_packages(),
     url='https://github.com/kentik/api-client',
