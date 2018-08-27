3b5c35f673ce2a8f13d9963c26523d6ec90288bd
Blake Caldwell <blake@kentik.com>
2018-08-27 21:03:04 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kentikapi',
-    version='0.1.7',
+    version='0.1.8',
     author='Blake Caldwell',
     packages=find_packages(),
     url='https://github.com/kentik/api-client',
